401bb1fefbe757c0604e5f32453a7a155e8eaa70
ShahJamal6160 <shahj6160@gmail.com>
2024-09-21 22:35:21 +0600

    edit all section

--- js/add-money.js
+++ js/add-money.js
@@ -7,11 +7,25 @@ document.getElementById('btn-add-money')
         const pinNumber = getInputFieldValueById('input-pin-number')
         // console.log(addMoney, pinNumber);
 
+        if(isNaN(addMoney)){
+            alert('failed to procese');
+            return;
+        }
+
         if(pinNumber === 1234){
             const balance = getTextValueById('account-balance');
             // console.log(balance, addMoney);
             const newBalance = balance + addMoney;
             document.getElementById('account-balance').innerText = newBalance;
+
+            // transaction section
+            const p = document.createElement('p')
+            p.classList.add('bg-blue-500')
+            p.innerText = `add-money: ${addMoney} tk. balance: ${newBalance}`
+            // console.log(p)
+            document.getElementById('transaction-container').appendChild(p);
+
+           
         }
         else{
             alert('wrong phone & pin Number')
--- js/cash-out.js
+++ js/cash-out.js
@@ -1,5 +1,5 @@
 document.getElementById('btn-cash-out')
-    .addEventListener('click', function(event){
+    .addEventListener('click', function (event) {
         event.preventDefault();
         // console.log('added cash out')
 
@@ -7,13 +7,34 @@ document.getElementById('btn-cash-out')
         const pinNumber = getInputFieldValueById('input-cash-out-pin');
         // console.log(cashOut, pinNumber);
 
-        if(pinNumber === 1234){
+        if(isNaN(cashOut)){
+            alert('failed to procese');
+            return;
+        }
+
+        if (pinNumber === 1234) {
             // console.log('added')
             const balance = getTextValueById('account-balance');
             const newBalance = balance - cashOut;
             document.getElementById('account-balance').innerText = newBalance;
+
+            //transaction section
+            const div = document.createElement('div')
+            div.classList.add('bg-yellow-300');
+            div.innerHTML = `
+                <h2 clss="text-3xl font-bold">Cash Out</h2>
+                <p> ${cashOut} Withdraw. New Balance ${newBalance}
+            `
+            // console.log(div);
+            document.getElementById('transaction-container').appendChild(div);
+
+            // const h1 = document.getElementById('h1')
+            // h1.classList.add('bg-blue-500')
+            // h1.innerText = `Cash Out: ${cashOut} withdraw, New Balance: ${newBalance}`
+            // document.getElementById('transaction-container').appendChild(h1);
+
         }
-        else{
+        else {
             alert('Access denied');
         }
     })
